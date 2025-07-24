5cd4c8c3b183d62a46dde8b5d88fc1d2260f5fa9
CrisKroos25 <cristianguzman0928@gmail.com>
2025-07-24 13:06:05 -0600

    formulario1
    ejemplo

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hola mundo")
+import streamlit as st
+
+st.title("Formulario de registro")
+
+with st.form("formulario_registro"):
+    id = st.text_input("Id ")
+    nombre = st.text_input("Nombre")
+
+    insertar = st.form_submit_button("Insertar")
+    start = st.form_submit_button("Start")
+    commit = st.form_submit_button("Commit")
+    roll = st.form_submit_button("Rollback")
